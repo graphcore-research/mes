4b34b1bcc5f54265a4fdd568aca14b36f6416a13
Tom Pollak <tomp@graphcore.ai>
2025-08-21 06:55:58 +0100

    more

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -19,16 +19,17 @@ wds = np.logspace(-2, 1, num=10)  # 1e-2 -> 10
 lrs = np.logspace(-4, -0.5, num=10)  # 1e-4 -> 5e-2
 acq_fun_params_list = [
     dict(lr=lr, wd=wd, max_iters=max_iters)
-    for lr, wd, max_iters in product(lrs, wds, [25])
+    for lr, wd, max_iters in product(lrs, wds, [50])
 ]  # default params
 acq_types = [
     "ves_mc_gamma",
     "ves_gamma",
     "expected_improvement",
     "random_search",
+    "ves_ramp",
 ]
 kernel_types = ["matern-3/2", "matern-5/2"]
-len_scales = [25]
+len_scales = [10, 25]
 n_dims = [2, 4]
 
 # n_x increases with dimensionality. E.g. in 2D n_x = 200 => 200**2 points.
